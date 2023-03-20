d867408618df76bcc5ef71f195a8d4350ade0728
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2023-03-20 10:01:02 +0000

    punto4, 5 incompleto

--- verifica/src/app/item-serietv/item-serietv.component.ts
+++ verifica/src/app/item-serietv/item-serietv.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SerietvService } from '../serietv.service';
 
 @Component({
   selector: 'app-item-serietv',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-serietv.component.css']
 })
 export class ItemSerietvComponent {
+routeObs!: Observable<ParamMap> 
+serie!: any
+constructor(
+  private route: ActivatedRoute;
+  private router: Router;
+  private service: SerietvService;
+  private location: Location;
+){}
 
+ngOnInIt()void:{
+this.routeObs= this.route.ParamMap;
+this.routeObs.subscribe(this.getRouterParam)
+}
+
+const getRouterParam= (params: ParamMap)=>
+{
+  let serieId = params.get('id');
+  console.log(serieId);
+  let Obs= this.service.getSerie(serieId!);
+  obs
+}
 }
--- verifica/src/app/serietv.service.ts
+++ verifica/src/app/serietv.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaderResponse } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,10 @@ SearchSerie(query:string){
   const url= 'https://api.tvmaze.com/search/shows?q={query}'
   let ObsSerie= this.http.get(url)
   return ObsSerie
+}
+
+GetSerie(id:string): Observable<any>{
+  const url= 'https://api.tvmaze.com/shows/{id}'
+  return this.http.get(url)
 }
 }
